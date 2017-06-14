e6b6337716d20bffa8e3fe576bcc87257b704d99
dennis <dnnsmoyo@gmail.com>
2017-06-14 20:20:57 +0200

    Changed HTML for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e7jd!!&u)(0g+e94+e-bojg*7k1m3)xc1cpv_n7+xzw7w6mw%!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'denyce.pythonanywhere.com']
 
 
 # Application definition
